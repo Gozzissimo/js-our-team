e5385b591b354c0bdb7f30f50e12571f328fe397
Gozzissimo <fantini.nico@gmail.com>
2021-12-09 19:05:53 +0100

    Stampa in pagina OK

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+// In allegato trovate un layout di base e un file js in cui è definito un array di oggetti che rappresentano i membro del team.
+// Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// Prendendo come riferimento la card di esempio presente nell’html, stampiamo dinamicamente una card per ogni membro del team.
+//   BONUS:
+// Utilizziamo gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team: cliccando sul pulsante “add” viene creato un nuovo oggetto, il quale viene inserito nell’array iniziale, e viene stampata una nuova card con tutte le informazioni inserite dall’utente.
+
 const team = [
   {
     name: 'Wayne Barnett',
@@ -30,3 +36,24 @@ const team = [
     image: 'barbara-ramos-graphic-designer.jpg',
   },
 ];
+
+
+// 1. Creo un ciclo per stampare le card in pagina
+const container = document.querySelector('.team-container');
+
+for (let i = 0; i < team.length; i++) {
+  const element = team[i];
+  
+  const cardTemplate = `
+  <div class="team-card">
+    <div class="card-image">
+      <img src="img/${element.image}"/>
+    </div>
+    <div class="card-text">
+      <h3>${element.name}</h3>
+      <p>${element.role}</p>
+    </div>
+  </div >;`
+
+  container.innerHTML += cardTemplate;
+}
